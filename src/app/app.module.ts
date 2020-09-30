feea899326f372eea9f735dded85c768df499f8d
Isaac Nadar <isaac.nadar@freightbro.com>
2020-09-30 10:05:04 +0530

    added firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,3 +19,17 @@ export const ROUTES: Routes = [];
   bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+/*
+const firebaseConfig = {
+  apiKey: "AIzaSyA3ZLIWmV2YafFP7hSmfyUtqdNNGW0OWU4",
+  authDomain: "fitness-app-b08ae.firebaseapp.com",
+  databaseURL: "https://fitness-app-b08ae.firebaseio.com",
+  projectId: "fitness-app-b08ae",
+  storageBucket: "fitness-app-b08ae.appspot.com",
+  messagingSenderId: "976270857360",
+  appId: "1:976270857360:web:ba2afa220b12a4ff4c7415"
+};
+
+https://fitness-app-b08ae.firebaseapp.com/
+*/
